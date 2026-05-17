84469bf781c852474267b03f3ab64988a6d03c6b
isha152002 <ishajain152002@gmail.com>
2026-05-17 12:24:41 +0530

    added tweet cleaning pipeline in preprocessing.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from utils.preprocessing import clean_tweet
+
+tweet="@user you people should go back 🤮 http://t.co/xyz #immigrants lmaooo &amp; KILL"
+print(clean_tweet(tweet))
---
+++ utils/preprocessing.py
@@ -0,0 +1,14 @@
+import re
+import html
+import emoji
+
+def clean_tweet(text):
+    text=html.unescape(text) # convert HTML entities first
+    text=text.lower() # lowercase
+    text=re.sub(r'http\S+|www\S+', '', text) # remove URLs
+    text=re.sub(r'@\w+', '', text) # remove mentions
+    text=re.sub(r'#(\w+)', r'\1', text) # remove # keep word
+    text=re.sub(r'(.)\1{2,}', r'\1', text) # normalise repeated chars
+    text=emoji.replace_emoji(text, replace='') # remove emojis
+    text=re.sub(r'\s+', ' ', text).strip() # clean whitespace
+    return text
